b87e521290f430faba8100199f35611244d6c86e
Adam Ryman <adamryman@gmail.com>
2018-06-30 23:47:23 -0700

    Clear more space

--- keyboards/ergodox_ez/keymaps/adamryman/keymap.c
+++ keyboards/ergodox_ez/keymaps/adamryman/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | PRGM |      |AltShf|      |  Alt |                                       |  Up  | Down |   [  |   ]  | ~L1  |
+ *   | PRGM |      |AltShf|      |  Alt |                                       |      |      |   [  |   ]  | ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 																					 TG(SYMB),    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_BSLS,
 																								  KC_H,   KC_J,  KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_ENT,
 																					 MEH_T(KC_NO),KC_N,   KC_M,  KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
-																										  KC_UP, KC_DOWN,KC_LBRC,KC_RBRC,          KC_FN1,
+																										  KC_NO, KC_NO  ,KC_LBRC,KC_RBRC,          KC_FN1,
 
 																					 KC_LALT,        CTL_T(KC_ESC),
 																					 KC_PGUP,
